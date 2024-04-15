badc09b8b8fa5633219018dd8c232f6739788b49
Tomas Dvorsky <tomas.dvorsky@gmail.com>
2024-04-15 18:10:35 +0200

    logging call received

--- src/main/java/cz/pts/ptstestapp/TestController.java
+++ src/main/java/cz/pts/ptstestapp/TestController.java
@@ -1,5 +1,7 @@
 package cz.pts.ptstestapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/hello")
     public String getHello() throws InterruptedException {
         Thread.sleep(1000L);
+        logger.info("Call received");
         return "Hello";
     }
 
